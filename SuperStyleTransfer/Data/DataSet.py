f40c760b65437e5d6d10c71e7161bf8ec3608cb1
farrellsc <farrellsc@163.com>
2018-10-31 10:51:42 -0400

    code structure

---
+++ SuperStyleTransfer/Data/DataSet.py
@@ -0,0 +1,7 @@
+import torch
+
+
+class DataSet(torch.utils.data.Dataset):
+    def __init__(self):
+        super(dataSet, self).__init__()
+        raise NotImplementedError
---
+++ SuperStyleTransfer/Model/CycleGAN.py
@@ -0,0 +1,7 @@
+from .BaseModel import BaseModel
+
+
+class CycleGAN(BaseModel):
+    def __init__(self):
+        super(CycleGAN, self).__init__()
+        raise NotImplementedError
