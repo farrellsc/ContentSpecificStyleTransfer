539152683a9f9f8246611f262828512298f58cd0
farrellsc <farrellsc@163.com>
2018-11-09 16:44:23 -0500

    minor

--- SuperStyleTransfer/Data/DataLoader.py
+++ SuperStyleTransfer/Data/DataLoader.py
@@ -1,4 +1,7 @@
-class DataLoader:
+import torch
+
+
+class DataLoader(torch.utils.data.DataLoader):
     def __init__(self):
         super(DataLoader, self).__init__()
         raise NotImplementedError
