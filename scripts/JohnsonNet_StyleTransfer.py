f3b7c747b0b73d38ab7d77cc9ef5b172bdcf6741
farrellsc <zhou.zhuang.farrell@gmail.com>
2018-11-10 04:01:42 +0000

    minor

--- scripts/JohnsonNet_StyleTransfer.py
+++ scripts/JohnsonNet_StyleTransfer.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     args = {
         "content_image": "/home/zz2590/SuperStyleTransfer/data/images/content-images/TrainingData/amber.jpg",
         "content_scale": None,
-        "model": "../models/JohnsonNet/epoch_2_Sat_Nov_10_01:09:42_2018_100000.0_10000000000.0.model",
-        "output_image": "/home/zz2590/SuperStyleTransfer/output/JohnsonNet/test2.jpg"
+        "model": "../models/JohnsonNet/candy.pth",
+        "output_image": "/home/zz2590/SuperStyleTransfer/output/JohnsonNet/test.jpg"
     }
     stylize(DotDict(args))
