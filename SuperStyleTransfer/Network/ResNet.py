bd23a5b56e90cd82107ab844864dbfacfbbf3b54
farrellsc <farrellsc@163.com>
2018-11-30 19:42:47 -0500

    hehe

--- SuperStyleTransfer/Network/ResNet.py
+++ SuperStyleTransfer/Network/ResNet.py
@@ -6,9 +6,10 @@ from ..NetComponents.UpSamplingLayer import UpSamplingLayer
 
 
 class ResNet(NetworkBase):
-    def __init__(self):
+    def __init__(self, n_blocks):
         # Initial convolution layers
         super(ResNet, self).__init__()
+        self.n_blocks = n_blocks
         self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1)
         self.in1 = torch.nn.InstanceNorm2d(32, affine=True)
         self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
@@ -16,11 +17,9 @@ class ResNet(NetworkBase):
         self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
         self.in3 = torch.nn.InstanceNorm2d(128, affine=True)
         # Residual layers
-        self.res1 = ResBlock(128)
-        self.res2 = ResBlock(128)
-        self.res3 = ResBlock(128)
-        self.res4 = ResBlock(128)
-        self.res5 = ResBlock(128)
+        self.res = []
+        for i in range(n_blocks):
+            self.res.append(ResBlock(128))
         # Upsampling Layers
         self.deconv1 = UpSamplingLayer(128, 64, kernel_size=3, stride=1, upsample=2)
         self.in4 = torch.nn.InstanceNorm2d(64, affine=True)
@@ -38,11 +37,8 @@ class ResNet(NetworkBase):
         y = self.relu(self.in1(self.conv1(x)))
         y = self.relu(self.in2(self.conv2(y)))
         y = self.relu(self.in3(self.conv3(y)))
-        y = self.res1(y)
-        y = self.res2(y)
-        y = self.res3(y)
-        y = self.res4(y)
-        y = self.res5(y)
+        for i in range(self.n_blocks):
+            y = self.res[i](y)
         y = self.relu(self.in4(self.deconv1(y)))
         y = self.relu(self.in5(self.deconv2(y)))
         y = self.deconv3(y)
